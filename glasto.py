e6ab79ecb03ab78ae36c3cd64313a99024ec89fa
Daniel <dbyrne@tactiq.co.uk>
2018-10-16 13:07:58 +0100

    Updateing to not wait for page load

--- glasto.py
+++ glasto.py
@@ -2,9 +2,8 @@ import time
 from worker import worker
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import threading
-
-
 
 
 if __name__ == '__main__':
@@ -13,6 +12,9 @@ if __name__ == '__main__':
     opts.add_argument("user-agent=Mozilla/5.1 (X11; Linux x86_64) AppleWebKit/537.37 (KHTML, like Gecko) Chrome/60.0.3112.51 Safari/537.37")
     #opts.add_argument("headless")
     #opts.add_argument("user-data-dir=C:\\Users\\Byrne\\AppData\\Local\\Google\\Chrome\\User Data\\Default");
+
+    caps = DesiredCapabilities().FIREFOX
+    caps["pageLoadStrategy"] = "none"
 
     proxys = [
     "46.5.252.70:8080",
@@ -25,7 +27,7 @@ if __name__ == '__main__':
     jobs = []
 
     for i in range(0, WORKER_COUNT):
-        p = threading.Thread(target=worker, args=(opts, proxys[i]))
+        p = threading.Thread(target=worker, args=(opts, proxys[i], caps))
         jobs.append(p)
         p.start()
 
--- worker.py
+++ worker.py
@@ -2,10 +2,11 @@ import time
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 
-def worker(opts, PROXY):
+def worker(opts, PROXY, caps):
     print("Creating Driver")
     opts.add_argument('--proxy-server=%s' % PROXY)
-    driver = webdriver.Firefox(firefox_options=opts)
+    driver = webdriver.Firefox(firefox_options=opts, desired_capabilities=caps)
+    none
 
     while True:
         try:
